7d59bceaff5088b116bb95ea471813e8e5c02e6b
Meriem Mostefai <mostefaimeriem@gmail.com>
2024-07-14 21:49:02 -0400

    Create server.php

---
+++ hangman/versions/v02/server.php
@@ -0,0 +1,9 @@
+/* You will expose the Yatzy game functionality through a JSON API, 
+and update your JavaScript to make AJAX calls to your newly created API.
+All game state should be stored on the PHP server (you can store that information 
+in the PHP $_SESSION) and your HTML/CSS/JavaScript should focus on displaying the current 
+state of the game (the JavaScript should NOT calculate scores or track progress, just display the current state).
+
+You will track a game leaderboard showing your top 10 scores. For simplicity, this information 
+can also be stored in the PHP $_SESSION. */
+
